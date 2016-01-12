f7cc3aa7fdb89ce3d04b7e43dbf27fbfc2b19b75
echangein <e.change.in@gmail.com>
2016-01-12 11:08:05 +1000

    need reverse checked

--- tst.py
+++ tst.py
@@ -6,13 +6,13 @@ import time, os, json, sys
 #from spec import Spec
 
 
-pair = 'ltc_rur'
+pair = 'ltc_btc'
 silent = False
 
 profitPercent = 1.1
-startPercent = 0.9
-deepPercent = 20
-totalInvest = 5000
+startPercent = 1
+deepPercent = 18
+totalInvest = 45.5
 activeOrdersCount = 5
 
 profitPrecision = 2
@@ -21,6 +21,15 @@ import config
 #from spec import Spec
 from cascade import Cascade
 import os.path
+
+cascadeFile = 'trades_btc_rur_btc_rur_key'
+#cascadeFile = 'trades_btc_usd_btc_usd_key'
+cascadeFile = 'trades_ltc_btc_ltc_btc_key'
+#cascadeFile = 'trades_ltc_rur_rur'
+#cascadeFile = 'trades_ltc_usd_usd'
+
+dirname, filename = os.path.split(os.path.abspath(__file__))
+cascadeFileName = dirname + '/../' + cascadeFile
 
 key = None
 secret = None
@@ -36,11 +45,24 @@ engine.setActiveOrdersCount(activeOrdersCount)
 
 engine.setProfitPrecision(profitPrecision)
 
-cascade = engine.createCascade(17.99258339, 666) #
+#cascade = engine.createCascade(17.99258339, 666) #
 
 #print(cascade)
 #print(cascade[0]['sellOrder']['operationAmount'])
+
+file = open(cascadeFileName, 'r+')
+cascade = json.load(file)
+file.close()
+
+
 engine.printCascade(cascade)
+
+if engine.needReverse(cascade):
+	print('need Reverse')
+else:
+	print('still wait')
 
 if len(cascade) > 0 and 'options' in cascade[0]:
 	print('cascade is revers')
+else:
+	print('cascade is normal')

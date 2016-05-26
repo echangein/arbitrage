d29304ed0cfa1b4a8ceaf87f9c5b1a9c6fb0b312
echangein <e.change.in@gmail.com>
2016-05-26 12:45:08 +1000

    concept designed in process

--- cscv2.py
+++ cscv2.py
@@ -11,23 +11,53 @@ if error:
 	saveCascadeFile(cascadeFileName, cascadeStruct)
 	quit()
 
-if needRestartCascadeStruct(cascadeStruct):
+# ================== restart cascade ================== #
+if not inWork(cascadeStruct) and needRestart(cascadeStruct):
+	cascadeStruct, error = cancelOrders(cascadeStruct)
+	if error:
+		reportCancelOrdersError()
+		saveCascadeFile(cascadeFileName, cascadeStruct)
+		quit()
+		
 	cascadeStruct = createCascadeStruct()
 	cascadeStruct, error = checkOrdersStatus(cascadeStruct)#can be errors
 	if error:
 		reportCheckOrdersStatusError()
 		saveCascadeFile(cascadeFileName, cascadeStruct)
 		quit()
-	
+# ================== restart cascade ================== #
+
+# ================== cascade get profit ================== #
 if hasProfit(cascadeStruct): #sell order complete
 	reportProfit(cascadeStruct)
-	removeCascadeFile(cascadeFileName) # need check executed next buy order
-	quit()
-	
-if not waitingProfit(cascadeStruct): #sell order is partial executed
-	cascadeStruct, error = createOrders(cascadeStruct)
-	if error:
-		reportCreateOrdersError()
-	cascadeStruct = shiftOrders(cascadeStruct)
+	if hasPartialExecution(cascadeStruct): # need check executed next buy order
+		cascadeStruct = resizeAfterProfit(cascadeStruct)
+	else:
+		cascadeStruct, error = cancelOrders(cascadeStruct)
+		if error:
+			reportCancelOrdersError()
+			saveCascadeFile(cascadeFileName, cascadeStruct)
+			quit()
+		if hasParnet(cascadeStruct): # for reverse
+			restoreParent(cascadeStruct)
+			quit()
+		else:
+			removeCascadeFile(cascadeFileName)
+			quit()
+# ================== cascade get profit ================== #
+
+# ================== create revers cascade ================== #
+# ================== create revers cascade ================== #
+
+# ================== create order sequence ================== #
+cascadeStruct, error = createOrders(cascadeStruct)
+if error:
+	reportCreateOrdersError()
+cascadeStruct = shiftOrders(cascadeStruct)
+
+cascadeStruct, error = moveProfitOrder(cascadeStruct)
+if error:
+	reportMoveProfitOrderError()
+# ================== create order sequence ================== #
 
 saveCascadeFile(cascadeFileName, cascadeStruct)
